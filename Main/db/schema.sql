fdf201ce1ecc44db9ee8fe47904edde9e88896df
Kalia Thao <kaliathao21@gmail.com>
2023-09-12 19:38:20 -0700

    Added foreign keys

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -3,22 +3,24 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
-CREATE TABLE department (
+CREATE TABLE Departments (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE Roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
-    salary,
-    department ID INT NOT NULL
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES Departments(id) ON DELETE SET NULL
 );
 
-CREATE TABLE employee (
+CREATE TABLE Employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NOT NULL
+    manager_id INT NOT NULL,
+    FOREIGN KEY (manager_id) REFERENCES Roles(id) ON DELETE SET NULL
 );
