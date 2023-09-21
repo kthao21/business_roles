ce48539072be04ddbced0dc3e3dff6de9ba77097
Kalia Thao <kaliathao21@gmail.com>
2023-09-21 10:25:52 -0700

    updated index.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,11 +4,14 @@ VALUES  ("Marketing"),
         ("Product development"),
         ("Operations");
 
-INSERT INTO roles ( title, id, department_id, salary )
+INSERT INTO roles (title, id, department_id, salary)
 VALUES  ("Product Management", 1, "Product development", 100,000),
         ("Marketing Communications", 2, "Marketing", 70,000),
         ("Sales Support", 3, "Sales and Support", 50,000),
         ("HR", 4, "Operations", 80,000 );
 
-INSERT INTO employees ( id, first_name, last_name, title, departments, salaries, manager)
-VALUES ()
+INSERT INTO employees (id, first_name, last_name, title, departments, salaries, manager)
+VALUES  (3, "John", "Doe", "Sales Support", "Sales and Support", 50,000, "Melissa"),
+        (1, "Jane", "Smith", "Product Management", "Product development", 100,000, "Joseph"),
+        (4, "Vanessa", "Hudgens", "HR", "Operations", 80,000, "Brandon"),
+        (2, "Donald", "Johnson", "Marketing Communications", "Marketing", 70,000, "Steve");
--- index.js
+++ index.js
@@ -1,63 +1,119 @@
 const inquirer = require('inquirer');
 
-inquirer.prompt ([
-        {
-            type: 'list',
-            message: 'What would you like to do?',
-            name: 'action',
-            choices: [
-                'View all departments',
-                'View all roles',
-                'View all employees',
-                'Add a department',
-                'Add a role',
-                'Add an employee',
-                'Update an employee role',
-                ]
-        },
-        {
-            type: 'input',
-            message: 'What is the name of the department?',
-            name: 'department',
-        },
-        {
-            type: 'input',
-            message: 'What is the name of the role?',
-            name: 'title',
-        },
-        {
-            type: 'input',
-            message: 'What is the salary of the role?',
-            name: 'salary',
-        },
-        {
-            type: 'input',
-            message: 'Which department does the role belong to?',
-            name: 'connection',
-        },
-        {
-            type: 'input',
-            message: `What is the employee's first name?`,
-            name: 'first',
-        },
-        {
-            type: 'input',
-            message: `What is the employee's last name?`,
-            name: 'last',
-        },
-        {
-            type: 'input',
-            message: `What is the employee's role?`,
-            name: 'role',
-        },
-        {
-            type: 'input',
-            message: `Who is the employee's manager?`,
-            name: 'manager',
-        },
-        {
-            type: 'input',
-            message: `Which employee's role do you want to update?`,
-            name: 'action',
+inquirer.prompt([
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'action',
+        choices: [
+            {
+                name: "View All Employees",
+                value: "VIEW_EMPLOYEES"
+            },
+            {
+                name: "View All Employees By Department",
+                value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
+            },
+            {
+                name: "View All Employees By Manager",
+                value: "VIEW_EMPLOYEES_BY_MANAGER"
+            },
+            {
+                name: "Add Employee",
+                value: "ADD_EMPLOYEE"
+            },
+            {
+                name: "Remove Employee",
+                value: "REMOVE_EMPLOYEE"
+            },
+            {
+                name: "Update Employee Role",
+                value: "UPDATE_EMPLOYEE_ROLE"
+            },
+            {
+                name: "Update Employee Manager",
+                value: "UPDATE_EMPLOYEE_MANAGER"
+            },
+            {
+                name: "View All Roles",
+                value: "VIEW_ROLES"
+            },
+            {
+                name: "Add Role",
+                value: "ADD_ROLE"
+            },
+            {
+                name: "Remove Role",
+                value: "REMOVE_ROLE"
+            },
+            {
+                name: "View All Departments",
+                value: "VIEW_DEPARTMENTS"
+            },
+            {
+                name: "Add Department",
+                value: "ADD_DEPARTMENT"
+            },
+            {
+                name: "Remove Department",
+                value: "REMOVE_DEPARTMENT"
+            },
+            {
+                name: "View Total Utilized Budget By Department",
+                value: "VIEW_UTILIZED_BUDGET_BY_DEPARTMENT"
+            },
+            {
+                name: "Quit",
+                value: "QUIT"
+            }
+        ]
+    }]).then(res => {
+        let choice = res.choice;
+
+        switch (choice) {
+            case "VIEW_EMPLOYEES":
+                viewEmployees();
+                break;
+            case "VIEW_EMPLOYEES_BY_DEPARTMENT":
+                viewEmployeesByDepartment();
+                break;
+            case "VIEW_EMPLOYEES_BY_MANAGER":
+                viewEmployeesByManager();
+                break;
+            case "ADD_EMPLOYEE":
+                addEmployee();
+                break;
+            case "REMOVE_EMPLOYEE":
+                removeEmployee();
+                break;
+            case "UPDATE_EMPLOYEE_ROLE":
+                updateEmployeeRole();
+                break;
+            case "UPDATE_EMPLOYEE_MANAGER":
+                updateEmployeeManager();
+                break;
+            case "VIEW_DEPARTMENTS":
+                viewDepartments();
+                break;
+            case "ADD_DEPARTMENT":
+                addDepartment();
+                break;
+            case "REMOVE_DEPARTMENT":
+                removeDepartment();
+                break;
+            case "VIEW_UTILIZED_BUDGET_BY_DEPARTMENT":
+                viewUtilizedBudgetByDepartment();
+                break;
+            case "VIEW_ROLES":
+                viewRoles();
+                break;
+            case "ADD_ROLE":
+                addRole();
+                break;
+            case "REMOVE_ROLE":
+                removeRole();
+                break;
+            default:
+                quit();
         }
-    ]);
+    })
