a95d8ac455ad36f0e3163325a61e43c91e50cd07
Kalia Thao <kaliathao21@gmail.com>
2023-10-05 14:54:40 -0700

    updates index.js and server.js

--- db/index.js
+++ db/index.js
@@ -7,21 +7,67 @@ class DB {
 
     //view all employees
     findAllEmployees () {
+        // id, first name, last name, title, department name, salary, managers firstname, managers last name
         return this.connection.promise().query(
-        "SELECT * from Employees")
+        "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, employee.manager_id  from role left join employee on employee.role_id = role.id")
+    }
+
+    //create employee
+    createEmployee (employee) {
+        return this.connection.promise().query(
+            "INSERT INTO employee SET ?", employee);
     }
     
+    //remove employee
+    removeEmployee (employeeId) {
+        return this.connection.promise().query(
+            "DELETE FROM Employee WHERE id = ?", employeeId);
+    }
+
+    //update employee role
+    updateEmployeeRole ( employeeId, roleId ) {
+        return this.connection.promise().query(
+        "UPDATE employee SET role_id = ? WHERE id = ?",
+        [ roleId, employeeId ]
+        );
+    }
+
+    //view all role
+    findAllRoles () {
+        // title, id, salary, department name
+        return this.connection.promise().query(
+        "SELECT role.title, role.id, role.salary, department.name from role left join department on role.department_id = department.id")
+    }
+
     //create role
     createRole (role) {
         return this.connection.promise().query(
-            "INSERT INTO Roles SET ?", role);
+            "INSERT INTO roles (title, salary, department_id) VALUES ( ?, ?, ?)");
     }
 
     //remove role
     removeRole (roleId) {
         return this.connection.promise().query(
-            "DELETE FROM Roles WHERE id = ?", roleId);
+            "DELETE FROM role WHERE id = ?", roleId);
     }
 
-    //
+    //view all departments
+    findAllDepartments () {
+        return this.connection.promise().query(
+        "SELECT * from department")
+    }
+
+    //create department
+    createDepartment (department) {
+        return this.connection.promise().query(
+            "INSERT INTO department (name) VALUES (?)", department);
+    }
+
+    //remove department
+    removeDepartment (departmentId) {
+        return this.connection.promise().query(
+            "DELETE FROM department WHERE id = ?", departmentId);
+    }
 }
+
+module.exports= new DB (connection);
--- server.js
+++ server.js
@@ -1,19 +1,90 @@
-const express = require('express');
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+const db = require('./db');
 
-//middleware?
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+function init() {
+    inquirer.prompt(
+        {
+            type: 'list',
+            message: '',
+            name: 'question',
+            choices: [
+                'view all departments',
+                'view all roles',
+                'view all employees',
+                'add a department',
+                'add a role',
+                'add an employee',
+                'update an employee role'
+            ]
+        }
+    ).then((response) => {
+        if (response.question === 'view all departments') {
+            viewAllDepartments();
+        } else if (response.question === 'view all roles') {
+            viewAllRoles();
+        } else if (response.question === 'view all employees') {
+            viewAllEmployees();
+        } else if (response.question === 'add a department') {
+            addDepartment();
+        } else if (response.question === 'add a role') {
+            addRole();
+        } else if (response.question === 'add an employee') {
+            addEmployee();
+        } else if (response.question === 'update an employee role') {
+            updateEmployee();
+        }
+    })
+}
 
-const db = mysql.createConnection (
-    {
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'company_db'
-    },
-    console.log('Connected to the company_db database.')
-);
+function viewAllDepartments() {
+    db.findAllDepartments().then(([data])=> {
+        console.table(data)
+        init()
+    })
+}
+
+function viewAllRoles() {
+    db.findAllRoles().then(([data])=> {
+        console.table(data)
+        init()
+    })
+}
+
+function addDepartment() {
+    db.createDepartment().then(([data])=> {
+        console.table(data)
+        init()
+    })
+}
+
+function viewAllEmployees() {
+    db.findAllEmployees().then(([data])=> {
+        console.table(data)
+        init()
+    })
+}
+
+function addRole() {
+    db.createRole().then(([data])=> {
+        console.table(data)
+        init()
+    })
+}
+
+function addEmployee() {
+    db.createEmployee().then(([data])=> {
+        console.table(data)
+        init()
+    })
+}
+
+function updateEmployee() {
+    db.updateEmployeeRole().then(([data])=> {
+        console.table(data)
+        init()
+    })
+}
+
+init()
